a9ccc0e9b57c1a0b087297111f952d7dc4e18a59
Khaerul Ilham <40401895+khaerulilham@users.noreply.github.com>
2018-08-07 19:57:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-$dir = scandir("/");
-print_r($dir);
-?>
+function daftar_file($dir){
+  if(is_dir($dir)){
+    if($handle = opendir($dir)){
+      //tampilkan semua file dalam folder kecuali
+      while(($file = readdir($handle)) !== false){
+        echo '<a target="_blank" href="'.$dir.$file.'">'.$file.'</a><br>'."\n";
+      }
+      closedir($handle);
+    }
+  }
+}
+//cara menggunakan
+daftar_file("/");?>
